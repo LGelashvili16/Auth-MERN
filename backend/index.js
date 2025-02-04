9a1942dc19dde33cbc2224d716dcb754388c05f2
Lado Gelashvili <ladogelashvili16@gmail.com>
2025-02-05 01:17:57 +0400

    feat: finish backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,6 +1,7 @@
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 import {
   PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
   VERIFICATION_EMAIL_TEMPLATE,
 } from "./emailTemplates.js";
 
@@ -60,5 +61,24 @@ export const sendResetPasswordEmail = async (email, resetURL) => {
   } catch (error) {
     console.error("Error sending reset password email", error);
     throw new Error(`Error sending reset password email: ${error}`);
+  }
+};
+
+export const sendResetSuccessEmail = async (email) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Password reset successful",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Reset Password",
+    });
+
+    console.log("Reset password email sent successfully", response);
+  } catch (error) {
+    console.log("Error in resetPassword", error);
+    throw new Error(`Error sending success reset password email: ${error}`);
   }
 };
